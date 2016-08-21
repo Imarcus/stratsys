6559f0946e1bab06d52e822f45ec3f1fbdcf9728
Marcus <marcus.i@ive.se>
2016-08-21 20:41:40 +0200

    workork

--- src/ClientController.java
+++ src/ClientController.java
@@ -1,5 +1,103 @@
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by Marcus on 2016-08-16.
  */
 public class ClientController {
+
+    Socket clientSocket;
+
+    @FXML
+    ListView<String> ageListView, styleListView;
+
+    @FXML
+    TextField lengthTextField;
+
+    @FXML
+    Label recommendLabel;
+
+    @FXML
+    public void initialize(){
+        styleListView.setDisable(true);
+        ageListView.setItems(FXCollections.observableArrayList (
+                SkiGuidePacket.Age.ZEROFOUR.getDescription(),
+                SkiGuidePacket.Age.FIVEEIGHT.getDescription(),
+                SkiGuidePacket.Age.NINEPLUS.getDescription()));
+
+        styleListView.setItems(FXCollections.observableArrayList (
+                SkiGuidePacket.Style.KLASSISK.name().toLowerCase(),
+                SkiGuidePacket.Style.FRISTIL.name().toLowerCase()));
+    }
+
+    /*
+    * Disables styleListView if NINEPLUS is not selected
+     */
+    public void onAgeListInteraction(){
+        if(ageListView.getSelectionModel().getSelectedItem().equals(SkiGuidePacket.Age.NINEPLUS.getDescription())){
+            styleListView.setDisable(false);
+        } else {
+            styleListView.setDisable(true);
+        }
+
+    }
+
+    public void onConfirmButtonPressed(){
+        SkiGuidePacket packet = createSkiGuidePacket();
+        sendPacket(packet);
+
+    }
+
+    private SkiGuidePacket createSkiGuidePacket(){
+        SkiGuidePacket.Age age;
+        String selectedAge = ageListView.getSelectionModel().getSelectedItem();
+        if(selectedAge.equals(SkiGuidePacket.Age.ZEROFOUR.getDescription())){
+            age = SkiGuidePacket.Age.ZEROFOUR;
+        } else if(selectedAge.equals(SkiGuidePacket.Age.FIVEEIGHT.getDescription())){
+            age = SkiGuidePacket.Age.FIVEEIGHT;
+        } else {//if(selectedAge.equals(SkiGuidePacket.Age.NINEPLUS.getDescription())){
+            age = SkiGuidePacket.Age.NINEPLUS;
+        }
+
+        SkiGuidePacket.Style style;
+        String selectedStyle = styleListView.getSelectionModel().getSelectedItem().toUpperCase();
+        if(selectedStyle.equals(SkiGuidePacket.Style.KLASSISK.name())){
+            style = SkiGuidePacket.Style.KLASSISK;
+        } else { //if (selectedStyle.equals(SkiGuidePacket.Style.FRISTIL.name())){
+            style = SkiGuidePacket.Style.FRISTIL;
+        }
+
+        return new SkiGuidePacket(age, style, Integer.parseInt(lengthTextField.getText()));
+
+    }
+
+    private void sendPacket(SkiGuidePacket packet){
+        try {
+            clientSocket = new Socket("127.0.0.1", 54555);
+            ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
+
+            outToServer.writeObject(packet);
+            receivePacket();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void receivePacket(){
+        try {
+            DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
+            //BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String result = dIn.readUTF();
+            recommendLabel.setText(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/SkiGuidePacket.java
@@ -0,0 +1,50 @@
+import java.io.Serializable;
+
+/**
+ * Created by Marcus on 2016-08-21.
+ */
+public class SkiGuidePacket implements Serializable{
+
+    public enum Age {
+        ZEROFOUR ("0-4 år"),
+        FIVEEIGHT ("5-8 år"),
+        NINEPLUS ("9+ år");
+
+        private String description;
+
+        Age(String text){
+            description = text;
+        }
+
+        public String getDescription(){
+            return description;
+        }
+    }
+
+    public enum Style {
+        KLASSISK,
+        FRISTIL;
+    }
+
+    private Age age;
+    private int length;
+    private Style style;
+
+    public SkiGuidePacket(Age pAge, Style pStyle, int pLength){
+        age = pAge;
+        length = pLength;
+        style = pStyle;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public Age getAge() {
+        return age;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
